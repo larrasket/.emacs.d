0f3a1000f841a21a862ea503945a8a413d551a4c
Salih Muhammed <lr0@gmx.com>
2023-09-06 15:41:02 +0300

    bump: :completion vertico

--- modules/completion/vertico/packages.el
+++ modules/completion/vertico/packages.el
@@ -4,19 +4,19 @@
 (package! vertico
   :recipe (:host github :repo "minad/vertico"
            :files ("*.el" "extensions/*.el"))
-  :pin "a28370d07f35c5387c7a9ec2e5b67f0d4598058d")
+  :pin "5780fac84f67c19cd6906077ecd636bac3c0c7ad")
 
 (package! orderless :pin "e6784026717a8a6a7dcd0bf31fd3414f148c542e")
 
-(package! consult :pin "fe49dedd71802ff97be7b89f1ec4bd61b98c2b13")
+(package! consult :pin "40607fb11ceaaa4d2d1c19673295923fd9cfa0e2")
 (package! consult-dir :pin "ed8f0874d26f10f5c5b181ab9f2cf4107df8a0eb")
 (when (and (modulep! :checkers syntax)
            (not (modulep! :checkers syntax +flymake)))
   (package! consult-flycheck :pin "3f2a7c17cc2fe64e0c07e3bf90e33c885c0d7062"))
-(package! embark :pin "9a44418c349e41020cdc5ad1bd21e8c77a429062")
+(package! embark :pin "46fb2f8bd90bc374d2cd0a55da3510a09070c859")
 (package! embark-consult :pin "9a44418c349e41020cdc5ad1bd21e8c77a429062")
 
-(package! marginalia :pin "866e50aee4f066b0903752c69b33e9b7cab93f97")
+(package! marginalia :pin "17fffc685731a1459fb5faa9e376b75ebe225d5c")
 
 (package! wgrep :pin "3132abd3750b8c87cbcf6942db952acfab5edccd")
 
